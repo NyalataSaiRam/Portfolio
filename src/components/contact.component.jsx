e17352473333608e4649ba612fb5ecb8c58d026b
unknown <nyalatasairam@gmail.com>
2024-06-28 20:03:22 +0530

    portfolio ninth commit

--- src/components/contact.component.jsx
+++ src/components/contact.component.jsx
@@ -7,15 +7,48 @@ import toast, { Toaster } from 'react-hot-toast';
 
 const Contact = () => {
 
-    const form = useRef(null);
+    const formElement = useRef(null);
 
     const sendEmail = (e) => {
         e.preventDefault();
 
+        let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; // regex for email
+        let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/; // regex for password
+        //formData
+        let form = new FormData(formElement.current);
+        let formData = {};
+
+        for (let [ key, value ] of form.entries()) {
+            formData[ key ] = value;
+        }
+
+
+
+        let { user_name, user_email, user_number, user_message } = formData;
+
+        if (user_name) {
+            if (user_name.length < 3) {
+                return toast.error("Fullname must me atleast 3 letters long");
+            }
+        }
+        if (!user_email.length) {
+            return toast.error("Invalid email ID");
+        }
+        if (!emailRegex.test(user_email)) {
+            return toast.error("Invalid Email");
+        }
+        if (!user_number.length || user_number.length < 10) {
+            return toast.error("Invalid Phone Number");
+        }
+        if (!user_message.length) {
+            return toast.error("Message cannot be empty");
+        }
+
+
         toast.promise(
 
             emailjs
-                .sendForm(import.meta.env.VITE_YOUR_SERVICE_ID, import.meta.env.VITE_YOUR_TEMPLATE_ID, form.current, {
+                .sendForm(import.meta.env.VITE_YOUR_SERVICE_ID, import.meta.env.VITE_YOUR_TEMPLATE_ID, formElement.current, {
                     publicKey: import.meta.env.VITE_YOUR_PUBLIC_KEY,
                 }),
 
@@ -48,7 +81,7 @@ const Contact = () => {
                 <div className='bg-blue bg-opacity-10 backdrop-blur-lg shadow-lg rounded-lg text-3xl w-[50%] flex flex-col justify-start p-6 mr-20 ml-20'>
                     <h1 className='text-blue tracking-wider sansita-bold-italic'>contact me</h1>
                     <div className='mt-4'>
-                        <form ref={form} onSubmit={sendEmail} className='flex flex-col justify-center' >
+                        <form ref={formElement} onSubmit={sendEmail} className='flex flex-col justify-center' >
                             <input required name='user_name' placeholder='Full name' className='mb-5 rounded p-1 text-blue sansita-regular bg-blue/10 pl-3 pr-3' type="text" />
                             <input required name='user_email' placeholder='Email' className='mb-5 rounded p-1 text-blue sansita-regular bg-blue/10 pl-3 pr-3' type="email" />
                             <input required name='user_number' placeholder='Phone No.' className='mb-5 rounded p-1 text-blue sansita-regular bg-blue/10 pl-3 pr-3' type="text" />
